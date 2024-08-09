fd62dbc7e68a9390f0ac9bcbeebc90858a494acf
Iterasys <correia@iterasys.com.br>
2024-08-08 22:56:41 -0300

    Métodos Post, Get, Put e Delete

--- test/api/pet.spec.js
+++ test/api/pet.spec.js
@@ -9,15 +9,61 @@ describe('API PetStore Swagger - Entidade Pet', () =>{
     // Atributos do grupo/describe
     const request = supertest('https://petstore.swagger.io/v2') // BaseURL
  
-    // Funções ou Métodos: Its
+     // Funções ou Métodos: Its
     it('POST Pet', () => {
+        
         // Atributos, Campos, Características, Configurações
-       
-        // Funções de Apoio (Opcional)
+        const pet = require('../../vendors/json/pet.json')
 
-        // Funções de Teste em Si
+        // Função de teste em si 
+        return request
+            .post('/pet')
+            .send(pet)
+            .then((res) => {
+                expect(res.statusCode).toBe(200)
+                expect(res.body.id).toBe(petId)
+                expect(res.body.name).toBe('Snoopy')
+                expect(res.body.category.name).toBe('dog')
+                expect(res.body.tags[0].name).toBe('vaccined')
+            })
+                // ---> Voltaremos 21:29
 
     }) // Final do método POST
+
+    it('GET Pet', () => {
+        return request
+        // .get('/pet/' + petID) // tradicional
+        .get(`/pet/${petId}`) // moderno: template literals
+        .then((res) => {
+    expect(res.statusCode).toBe(200)
+            expect(res.body.id).toBe(petId)
+            expect(res.body.status).toBe('available')
+        })
+    })
+
+    it('PUT Pet', () => {
+        const pet = require('../../vendors/json/petput.json')
+
+        return request
+            .put('/pet')
+            .send(pet)
+            .then((res) => {
+                expect(res.statusCode).toEqual(200)
+                expect(res.body.status).toEqual('sold')
+            })
+
+    })
+
+    it('DELETE Pet', () => {
+        return request
+            .delete(`/pet/${petId}`)
+            .then((res) => {
+                expect(res.statusCode).toEqual(200)
+                expect(res.body.code).toEqual(200)
+                expect(res.body.message).toBe(petId.toString())
+            })
+    })
+
 
 // Oi
 }) // termina a describe
