c0689044a4a4ad4e5e7d4b6889d26d3c02f8da36
Iterasys <correia@iterasys.com.br>
2024-08-13 22:28:48 -0300

    Versões Data Driven

--- test/api/pet.spec.js
+++ test/api/pet.spec.js
@@ -8,7 +8,8 @@ describe('API PetStore Swagger - Entidade Pet', () =>{
 
     // Atributos do grupo/describe
     const request = supertest('https://petstore.swagger.io/v2') // BaseURL
- 
+    const massa1 = require('../../vendors/json/massaPet')
+
      // Funções ou Métodos: Its
     it('POST Pet', () => {
         
@@ -26,9 +27,43 @@ describe('API PetStore Swagger - Entidade Pet', () =>{
                 expect(res.body.category.name).toBe('dog')
                 expect(res.body.tags[0].name).toBe('vaccined')
             })
-                // ---> Voltaremos 21:29
-
+                
     }) // Final do método POST
+
+    // Método POST que lê e cria 3 registros
+
+    it.each(massa1.array.map(elemento => [
+        elemento.nomePet,
+        elemento.idPet,
+        elemento.nomeCategoria,
+        elemento.idCategoria
+    ]))
+    ('POST Pet Data Driven Simples: %s', (nomePet, idPet, nomeCategoria, idCategoria) => {
+        
+        // Atributos, Campos, Características, Configurações
+        const pet = require('../../vendors/json/pet.json')
+
+        // Substituimos os campos que queremos personalizar através da massa
+        pet.id = idPet
+        pet.name = nomePet
+        pet.category.id = idCategoria
+        pet.category.name = nomeCategoria
+
+        // Função de teste em si 
+        return request
+            .post('/pet')
+            .send(pet)
+            .then((res) => {
+                expect(res.statusCode).toBe(200)
+                expect(res.body.id).toBe(idPet)
+                expect(res.body.name).toBe(nomePet)
+                expect(res.body.category.name).toBe(nomeCategoria)
+                expect(res.body.tags[0].name).toBe('vaccined')
+            })
+                
+    }) // Final do método POST
+
+    // Métodos Get, Put e Delete Simples
 
     it('GET Pet', () => {
         return request
@@ -64,7 +99,74 @@ describe('API PetStore Swagger - Entidade Pet', () =>{
             })
     })
 
+    // Testes Data Driven do CRUD (POST, GET, PUT e Delete)
+    massa1.array.forEach(({ nomePet, idPet, nomeCategoria, idCategoria }) => {
+        it(`POST Pet Data Driven ForEach - ${nomePet}`, () => {
+            // Atributos, Campos, Características, Configurações
+            const pet = require('../../vendors/json/pet.json')
 
-// Oi
+            // Substituimos os campos que queremos personalizar através da massa
+            pet.id = idPet
+            pet.name = nomePet
+            pet.category.id = idCategoria
+            pet.category.name = nomeCategoria
+
+            // Função de teste em si 
+            return request
+                .post('/pet')
+                .send(pet)
+                .then((res) => {
+                    expect(res.statusCode).toBe(200)
+                    expect(res.body.id).toBe(idPet)
+                    expect(res.body.name).toBe(nomePet)
+                    expect(res.body.category.name).toBe(nomeCategoria)
+                    expect(res.body.tags[0].name).toBe('vaccined')
+                })
+
+        }) // termina o Post
+
+        it(`GET Pet Data Driven ForEach - ${nomePet}`, () => {
+            return request
+            // .get('/pet/' + petID) // tradicional
+            .get(`/pet/${idPet}`) // moderno: template literals
+            .then((res) => {
+                expect(res.statusCode).toBe(200)
+                expect(res.body.id).toBe(idPet)
+                expect(res.body.status).toBe('available')
+            })
+        })
+    
+        it(`PUT Pet Data Driven ForEach - ${nomePet}`, () => {
+            const pet = require('../../vendors/json/petput.json')
+    
+            // Substituimos os campos que queremos personalizar através da massa
+            pet.id = idPet
+            pet.name = nomePet
+            pet.category.id = idCategoria
+            pet.category.name = nomeCategoria
+
+            return request
+                .put('/pet')
+                .send(pet)
+                .then((res) => {
+                    expect(res.statusCode).toEqual(200)
+                    expect(res.body.status).toEqual('sold')
+                })
+    
+        })
+    
+        it(`DELETE Pet Data Driven ForEach - ${nomePet}`, () => {
+            return request
+                .delete(`/pet/${idPet}`)
+                .then((res) => {
+                    expect(res.statusCode).toEqual(200)
+                    expect(res.body.code).toEqual(200)
+                    expect(res.body.message).toBe(idPet.toString())
+                })
+        })
+
+
+    }) // termina o For Each da massa
+
 }) // termina a describe
 
---
+++ vendors/json/massaPet.js
@@ -0,0 +1,21 @@
+module.exports.array = [
+    {
+      "nomePet": "Bolt",
+      "idPet": 173812102,
+      "nomeCategoria": "Dog",
+      "idCategoria": 1
+    },
+    {
+      "nomePet": "Spike",
+      "idPet": 173812103,
+      "nomeCategoria": "Dog",
+      "idCategoria": 1
+    },
+    {
+      "nomePet": "Bichento",
+      "idPet": 173812104,
+      "nomeCategoria": "Cat",
+      "idCategoria": 2
+    }
+  ]
+
